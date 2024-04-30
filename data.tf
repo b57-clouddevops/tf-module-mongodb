d8809449ae593bcc7e182c17f36abcb9cd7cce38
CodingManoj <CodingManoj@gmail.com>
2024-04-29 20:56:28 -0400

    Auto Push

--- data.tf
+++ data.tf
@@ -7,3 +7,13 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+# Extracts the secret's metadata
+data "aws_secretsmanager_secret" "secrets" {
+  name     = "robot/secrets"
+}
+
+# Extracting the secret version from the secret
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
--- docdb.tf
+++ docdb.tf
@@ -2,8 +2,8 @@
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "roboshop-${var.ENV}-docdb"
   engine                  = var.DOCDB_ENGINE_VERSION
-  master_username         = "admin1"
-  master_password         = "RoboShop1"
+  master_username         = local.DOCDB_USERNAME
+  master_password         = local.DOCDB_PASSWORD
 #   backup_retention_period = 5             # In Prod we would enable this 
 #   preferred_backup_window = "07:00-09:00" 
   skip_final_snapshot     = true
