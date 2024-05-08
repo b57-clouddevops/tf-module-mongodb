d055cedaf9fd85ca9c1f35e4cf88b98146373120
CodingManoj <CodingManoj@gmail.com>
2024-05-07 21:10:23 -0400

    Auto Push

--- locals.tf
+++ locals.tf
@@ -2,4 +2,3 @@ locals {
   DOCDB_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]  
   DOCDB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_PASSWORD"]
 }
-
